e3a33fa159cf3432468a41b3f7e0115130a000ff
Niharika Chenna <niharikachenna567@gmail.com>
2022-12-07 19:45:08 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Axios from 'axios'
 import { YOUR_APP_ID, YOUR_APP_KEY  } from './constants';
 import Card from './Card';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
@@ -17,40 +17,46 @@ function App() {
   // function sample(){
   //   console.log("hellooo")
   // }
-
+  useEffect(()=>{
+    getReceipeInfo()
+ },[])
 
   const getReceipeInfo = async() =>{
     var result = await Axios.get(url);
     setApiData(result.data.hits)
+    console.log(result.data)
   }
   return (
     <div className='container'>
-      <h1>Food Receipe</h1>
-      <form className='form-container'>
-        <input type="text" placeholder='enter food' className='search_field' onChange={(e) => {setSearchValue(e.target.value)}}/>
-      <select className='select-meal' onChange={(e) =>setMealType(e.target.value)}>
-        <option >meal type</option>
-        <option >breakfast</option>
-        <option>lunch</option>
-        <option>snack</option>
-        <option>dinner</option>
-     </select>
-     <select className='select-diet' onChange={(e) =>setdietType(e.target.value)}>
-        <option >diet type</option>
-        <option >balanced</option>
-        <option>high protien</option>
-        <option>high fiber</option>
-        <option>low fat</option>
-        <option>low carb</option>
-        <option>low sodium</option>
-     </select>
-        {/* <input type="text" placeholder='enter food type' className='search_field' onChange={(e) => {setFoodType(e.target.value)}}/> */}
-        <input type="button" value="Search" className='submit_btn' onClick={getReceipeInfo}/>
-      </form>
+      <div className='bg-container'>
+            <h1 className='name'>Food <span>Recipe</span></h1>
+            <p className='para'>"A <span>Recipe</span> has no soul, We Bring soul to the <span>Recipe</span>."</p>
+            <form className='form-container'>
+              <input type="text" placeholder='enter food' className='search_field' onChange={(e) => {setSearchValue(e.target.value)}}/>
+            <select className='select-meal' onChange={(e) =>setMealType(e.target.value)}>
+              <option >meal type</option>
+              <option >breakfast</option>
+              <option>lunch</option>
+              <option>snack</option>
+              <option>dinner</option>
+           </select>
+           <select className='select-diet' onChange={(e) =>setdietType(e.target.value)}>
+              <option >diet type</option>
+              <option >balanced</option>
+              <option>high protien</option>
+              <option>high fiber</option>
+              <option>low fat</option>
+              <option>low carb</option>
+              <option>low sodium</option>
+           </select>
+              {/* <input type="text" placeholder='enter food type' className='search_field' onChange={(e) => {setFoodType(e.target.value)}}/> */}
+              <input type="button" value="Search" className='submit_btn' onClick={getReceipeInfo}/>
+            </form>
+            </div>
       <div className='card-container'>
-      {apiData.map(val => {
-        return <Card image ={val.recipe.image} label={val.recipe.label}/> }
-      )}
+          {apiData.map(val => {
+             return <Card image ={val.recipe.image} label={val.recipe.label} weight={val.recipe.totalWeight} source={val.recipe.source}/> }
+          )}
       </div>
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,9 @@ function Card(props){
         <div className="card">
             <img src={props.image} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">{props.label}</h5>
+                <h3 className="card-title">{props.label}</h3>
+                <h4 className='source'>{`source:${props.source}`}</h4>
+                <h5 className="weight">{`totalweight:${props.weight}`}</h5>
             </div>
         </div>
     )
